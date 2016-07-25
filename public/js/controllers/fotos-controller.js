fa8ddfeb5e8a9ae717851123d40517a5526aa0a9
diemancini <diemancini@gmail.com>
2016-07-25 18:03:26 -0300

    Repeat e renderizar imagens com json funcionando corretamente atualizado

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,24 +1,5 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
-	$scope.fotos = [];
-		/*{
-			titulo: 'Leão',
-			url: 'http://img1.topimagens.com/ti/leoes/leoes_002.jpg'
-		},
-		{
-			titulo: 'Leão 2',
-			url: 'http://img1.topimagens.com/ti/leoes/leoes_002.jpg'
-		},
-		{
-			titulo: 'Leão 3',
-			url: 'http://img1.topimagens.com/ti/leoes/leoes_002.jpg'
-		},
-		{
-			titulo: 'Leão 4',
-			url: 'http://img1.topimagens.com/ti/leoes/leoes_002.jpg'
-		}
-	];*/
-
 	/*var promise = $http.get('v1/fotos');
 	promise.then(function(retorno) {
 		$scope.fotos = retorno.data;
