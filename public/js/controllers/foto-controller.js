66451ae9bd210888436c893930551826dfd54e31
diemancini <diemancini@gmail.com>
2016-07-26 14:21:43 -0300

    Validações de cadastro de foto feitas com sucesso

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,8 +1,21 @@
-angular.module('alurapic').controller('FotoController', function($scope) {
+angular.module('alurapic').controller('FotoController', function($scope, $http) {
 
 	$scope.foto = {};
+	$scope.mensagem = '';
 
 	$scope.submeter = function() {
-		console.log($scope.foto);
+		if ($scope.formulario.$valid) {
+			console.log($scope.foto);
+			$http.post('v1/fotos', $scope.foto)
+			.success(function() {
+				$scope.foto = {};
+				$scope.mensagem = 'Foto incluída com sucesso';
+				console.log('Foto cadastrada com sucesso')
+			})
+			.error(function(erro) {
+				$scope.mensagem = 'Foto não incluída';
+				console.log(erro);
+			});
+		}
 	}
 });
