af71a08d14c9bacb3e2f8fc1eaec6c0d821c75c8
diemancini <diemancini@gmail.com>
2016-07-25 23:02:10 -0300

    Configurando rotas no angularJs

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,19 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider, $locationProvider) {
+
+	//Esse artefato (classe) serve para tirar o símbolo '#' de cada rota do angularJs.
+	//Para funcionar, o backend precisa estar configurado.
+	$locationProvider.html5Mode(true);
+
+	$routeProvider.when('/fotos', {
+		templateUrl: 'partials/principal.html',
+		controller: 'FotosController'
+	});
+
+	$routeProvider.when('/fotos/new', {
+		templateUrl: 'partials/foto.html'
+		//controller: 'FotosController'
+	});
+
+	$routeProvider.otherwise({ redirectTo: '/fotos'});
+});
